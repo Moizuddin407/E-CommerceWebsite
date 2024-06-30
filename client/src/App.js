00cf4d1704586c99ec6adfeeda026ecbc6089115
Moiz Uddin M. Abdullah <moiz211j@gmail.com>
2024-06-30 14:03:10 +0500

    Pages and components added. Bootstrap integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
-import Layout from'./components/layout/Layout.js'
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage.js'
+import About from './pages/About.js'
+
+import Contact from './pages/Contact.js'
+import NotFound from './pages/Pagenotfound.js'
+import Policy from './pages/Policy.js'
 
 function App() {
   return (
    <>
-    <Layout>
-      <h1>Ecommerce App</h1>
-    </Layout>
+    <Routes>
+      
+      <Route path='/' element = {<HomePage/>}/>
+      <Route path='/about' element = {<About/>}/>  
+      <Route path='/contact' element= {<Contact/>}/>
+      <Route path = '/policy' element = {<Policy/>}/>
+      {/* For page not found */}
+      <Route path = '/*' element = {<NotFound/>}/>
+    </Routes>
    </>
   );
 }
+
+
 
 export default App;
---
+++ client/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const Contact = () => {
+  return (
+   <Layout>
+    <h1>Privacy policy page</h1>
+   </Layout>
+  )
+}
+
+export default Contact
